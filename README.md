976d758ab7cd1247c03725c800e1ebe24f8a9f4c
fmorriso <frederick.morrison@outlook.com>
2024-10-20 15:46:50 -0600

    upgrade to PySimpleGUI 5.0.7

--- README.md
+++ README.md
@@ -7,13 +7,15 @@
 | Python      |   3.13.0 |
 | VSCode      |   1.94.0 |
 | PyCharm     | 2024.2.3 |
-| PySimpleGUI |    5.0.6 |
+| PySimpleGUI |    5.0.7 |
 
 ## References
 * [PySimpleGUI](https://pysimplegui.com/)
 
 ## Change History
 
-| Date       | Description                             |
-|:-----------|:----------------------------------------|
-| 2024-10-07 | Initial creation                        |
+| Date       | Description                  |
+|:-----------|:-----------------------------|
+| 2024-10-07 | Initial creation             |
+| 2024-10-20 | Upgrade to PySimpleGUI 5.0.7 |
+
--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import sys
+import PySimpleGUI as sg
 
 
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
+def main():
+
+    # All the stuff inside your window.
+    layout = [[sg.Text("What's your name?")],
+              [sg.InputText()],
+              [sg.Button('Ok'), sg.Button('Cancel')]]
+
+    # Create the Window
+    window = sg.Window('Hello Example', layout)
+
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+
+        # if user closes window or clicks cancel
+        if event == sg.WIN_CLOSED or event == 'Cancel':
+            break
+
+        print('Hello', values[0], '!')
+
+    window.close()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(f'Python version {get_python_version()}')
+    main()
